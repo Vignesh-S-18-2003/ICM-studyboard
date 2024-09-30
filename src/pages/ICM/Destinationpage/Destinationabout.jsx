6c73e3841da3edd20a1a3aa20559a393a84d8596
Vignesh-S-18-2003 <vignesh12a240@gmail.com>
2024-09-30 19:52:56 +0530

    react

---
+++ src/pages/ICM/Destinationpage/Destinationabout.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Destinationabout() {
+  return (
+    <div>
+      <div className="container">
+          <div className="roww wow fadeInUp" data-wow-delay="500ms">
+            <div className="col-xl-12">
+              <div className="ui-content mt45 mb60">
+                <h1 className="mb20 text-center pb90">
+                Welcome To Australia!
+                </h1>
+                <h5 className="mb25 ff-heading text">
+                Australia offers an exceptional education system, home to some of the world's top-ranked universities known for cutting-edge research and innovative teaching methods. Students benefit from a diverse and inclusive environment, with opportunities to interact with peers from all over the globe. </h5>
+                <h5 className="mb25 ff-heading text">
+                Beyond academics, Australia provides a high quality of life, with vibrant cities, stunning natural landscapes, and a strong support system for international students. The country’s flexible post-study work options make it a great choice for those seeking to build a global career. </h5>
+                <h5 className='mb25 ff-heading text'>Studying in Australia not only enhances your academic credentials but also enriches your personal growth and life experiences.</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Destinationabout
--- src/pages/ICM/Destinationpage/index.jsx
+++ src/pages/ICM/Destinationpage/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import DestinationMain from './Destinationmain'
 import Header17 from '@/components/header/Header17'
+import Destinationabout from './Destinationabout'
 
 function Destinationpage() {
   return (
     <div>
         <Header17/>
       <DestinationMain/>
+      <Destinationabout/>
+      
     </div>
   )
 }
